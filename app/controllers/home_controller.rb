8306e4299036fafc2ff65d8572aa5314e77d77a1
Danielle <danielleheckman5@gmail.com>
2014-11-08 17:39:22 -0500

    using httpparty instead of js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,15 @@ class HomeController < ApplicationController
 
     def index
       if logged_in?
-        fs_auth = current_user.authorizations.find_by(provider: :foursquare)
+        response = HTTParty.get("https://api.foursquare.com/v2/venues/search?near=NewYork&client_id=4N23RFF1XUH1AA3AWBJFRPVR1SSMZU4FDU3PW1ADP5HTXHF4&client_secret=YHWQ322EJ2D5NMLKX5PY0432ZOZJV4W2XTK1ZUT315EJEIFD&v=20140806")
+        @v = JSON.parse(response.body)
+        @venues = @v["response"]["venues"]
 
       end
     end
 
-    def venues_search
-      if params[:name]
-        @venues = foursquare.venues.search(:ll => "40.7236307,-73.9999479")
-      end
-    end
+  private
+
+
 
 end
